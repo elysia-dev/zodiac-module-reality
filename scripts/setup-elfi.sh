4adafb4cb490d35e506345c6bd1e400dc0a81e3c
qpzm <lhm1442@gmail.com>
2023-07-25 18:55:58 +0900

    Deploy RealityModuleERC20 for EL, ELFI

--- scripts/setup-elfi.sh
+++ scripts/setup-elfi.sh
@@ -9,3 +9,6 @@ yarn hardhat --network cypress setup \
   --iserc20 true \
   --timeout 3600 \
   --cooldown 3600; # for one hour
+
+# Using the account: 0x45129A8fe7Dee4e6E16E738be673984DA684371F
+# Module deployed to: 0x063e6c98205b94abB7A303edd3FEd42CEC5B68Cd
